9bb13e7cdc9bcd0bb291d7729cb6b6ba219b842f
Radek Micek <radek.micek@gmail.com>
2024-02-24 12:10:22 +0100

    Chapter 10

---
+++ Chap10/Program.fs
@@ -0,0 +1,19 @@
+﻿
+open System
+
+open Common
+
+do
+    title "Exercise 1"
+
+    printfn "Thermometer is showing lower temperatures than actual."
+
+do
+    title "Exercise 2"
+
+    let normal = 98.6
+    let myAverage = (97.5 + 98.) / 2.
+    let childAverage = (100. + 99.5 + 99.6 + 100.2) / 4.
+    printfn $"Because I believe I have normal temperature and thermometer showed on average %f{myAverage}"
+    printfn $"which is %f{normal - myAverage} lower than normal then we have to adjust child average %f{childAverage}"
+    printfn $"by the same amount which gives %f{childAverage + normal - myAverage}"
